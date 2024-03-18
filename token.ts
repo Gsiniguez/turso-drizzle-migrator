98d8079d825f6ff98a8452d68f472b7deff46aab
Gonzalo Iniguez <gonza.ini94@gmail.com>
2024-03-18 16:04:42 -0300

    fix

--- token.ts
+++ token.ts
@@ -1,5 +1,5 @@
 const ORGANIZATION = 'YOUR-ORGANIZATION-NAME';
-export const DB_URL = 'libsql://main-${ORGANIZATION}.turso.io';
+export const DB_URL = `libsql://main-${ORGANIZATION}.turso.io`;
 export const DB_URL_TENANT = (tenant: string) =>
   `libsql://${tenant}-${ORGANIZATION}.turso.io`;
 
